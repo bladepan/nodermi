7866e826c37355fda3f262eb450f74714a06a378
Blade Pan <blade@msn.cn>
2015-02-05 15:36:31 -0500

    discard coffeescript

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 cd $i_scriptDir
 
-rm -rf lib
-node_modules/coffee-script/bin/coffee --output lib --compile src
+# rm -rf lib
+# node_modules/coffee-script/bin/coffee --output lib --compile src
